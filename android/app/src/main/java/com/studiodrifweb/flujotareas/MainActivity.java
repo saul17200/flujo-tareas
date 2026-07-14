8eb5ad0c037fe86eb5c580982309b345e14000fa
Osmar  Hernández <osmarhernandez@MacBook-Air-de-Osmar.local>
2026-07-14 00:32:42 -0600

    Prepara Drif Notion para modulo academico

---
+++ android/app/src/main/java/com/studiodrifweb/flujotareas/MainActivity.java
@@ -0,0 +1,5 @@
+package com.studiodrifweb.flujotareas;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli"
+
+const config: CapacitorConfig = {
+  appId: "com.studiodrifweb.flujotareas",
+  appName: "Drifnotes",
+  webDir: "dist",
+}
+
+export default config
